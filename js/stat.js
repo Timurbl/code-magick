e7ee129a4e5c18b41067c51f6e6cf14047c41c48
Timur <timurbl@mail.ru>
2019-08-14 11:45:02 +0300

    Список результатов upd

--- js/stat.js
+++ js/stat.js
@@ -6,7 +6,7 @@ const CLOUD_Y = 10;
 const COLUMN_WIDTH = 40;
 const GIST_HEIGHT = 150;
 const COLUMN_DISTANCE = 50;
-const COLUMN_SELF_COLOR = 'rgba(255, 0, 0, 1)';
+const COLUMN_SELF_COLOR = '#ff0000';
 
 const renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -23,9 +23,8 @@ const getMaxElement = function(array) {
   return maxElement
 };
 
-const renderColor = function(red, green, blue) {
+const renderBlueColor = function() {
   let randomColor = Math.floor(Math.random() * 255);
-  console.log(`#0000${randomColor.toString(16)}`);
   return `#0000${randomColor.toString(16)}`
 };
 
@@ -45,10 +44,10 @@ window.renderStatistics = function (ctx, names, times) {
     let columnY = CLOUD_Y + CLOUD_HEIGHT - columnHeight;
     const FONT_HEIGHT = 16;
     if (names[i] === 'Вы') {
-      ctx.fillStyle = '#ff0000'
+      ctx.fillStyle = COLUMN_SELF_COLOR;
     }
     else {
-      ctx.fillStyle = renderColor(0,0,255);
+      ctx.fillStyle = renderBlueColor()
     }
     ctx.fillText(names[i], columnX, columnY - FONT_HEIGHT);
     ctx.fillRect(columnX, columnY, COLUMN_WIDTH, columnHeight)
